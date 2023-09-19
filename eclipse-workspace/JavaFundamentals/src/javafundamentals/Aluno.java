f939994cac89e82a74992866119b72c059d50e83
Hian <contato.hian@outlook.com>
2023-09-19 13:23:03 -0300

    atualização arquivos

---
+++ eclipse-workspace/JavaFundamentals/src/javafundamentals/Aluno.java
@@ -0,0 +1,16 @@
+package javafundamentals;
+
+public class Aluno {
+	String nome;
+	int identificacao;
+	String curso;
+	double media;
+	
+	public void definirNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String obterNome() {
+		return nome;
+	}
+}
